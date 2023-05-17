f4f2f9771c8aa17f0599943f2f70ae92d489ccbf
ianpatricck <ianpatrick499@gmail.com>
2023-05-17 18:42:05 -0300

    :sparkles: decimalToBinary method has been implemented

--- src/usecases/converter/ConverterUseCase.ts
+++ src/usecases/converter/ConverterUseCase.ts
@@ -3,7 +3,28 @@ import IConverterUseCase from './IConverterUseCase';
 class ConverterUseCase implements IConverterUseCase {
 
   decimalToBinary(decimal: number): number | undefined {
-    return decimal;
+
+    var results: number[] = [];
+    var counter = decimal;
+
+    do {
+
+      counter--;
+      
+      let rest = decimal % 2;
+      decimal = Math.floor(decimal / 2);
+
+      results.push(rest);
+      if (decimal < 2) results.push(decimal); 
+
+    } while (counter >= 2 && decimal >= 2);
+
+    return Number(results.reverse().join(''));
+  }
+
+  binaryToDecimal(binary: number): number | undefined {
+    // ...
+    return binary;
   }
 
 }
